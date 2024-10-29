d642e241a12e0634c504753aa59d017d12e170b9
abyss <breadsoobinie.0512@gmail.com>
2024-10-28 23:23:30 -0600

    Codigo de la biblioteca de parametros escrito y documentado

--- parametrosPila.h
+++ parametrosPila.h
@@ -1,13 +1,21 @@
+
+//Biblioteca que define parametros para la pila
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//Definir parametros para la pila:
+#define PILA_SIZE 100
 
-//Tamaño
-//Un arreglo con un limite de caracteres para que no se desborde
-//Pensandolo mejor, un stack evitaría un desbordammiento, el array puede quedar pequeño
+typedef struct {
+    char pila[PILA_SIZE]; // Tamaño de la pila
+    int tope; // Inicio o tope de la pila
+} Pila;
 
-//Punto/indice de inicio
-//Un indice que apunte siempre a quien esta al frente o al tope de la pila
+void inicioPila(Pila *s) {         //Indica el inicio de nuestra pila, de donde partimos
+    s->tope = -1;       //Indice de nuestro tope en la pila
+}
 
-//pila vacia
-//identificador de pila vacia para el indice
+int esVacio(Pila *s) {      //Lee si nuestra pila esta vacía
+    return s->tope == -1;       //Regresa nuestro indice en la posición anterior (si es que esta no esta vacia)
+}
