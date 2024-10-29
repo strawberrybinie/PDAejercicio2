a4b9c3205b5d39f58376e62ce3d4ced10ef67aff
strawberrybinie <breadsoobinie.0512@gmail.com>
2024-10-28 22:39:58 -0600

    Se sube el pseudo codigo del proyecto

---
+++ PDAejercicio2.c
@@ -0,0 +1,30 @@
+/*
+Alumno: Ocampo Canales Kaory Marlene
+Fecha de entrega: 29 de octubre
+Fecha de actualización: 28 de octubre
+Nombre del archivo: PDAejercicio2
+*/
+
+#include <stdio.h>
+
+// Esqueleto del programa
+
+/*
+clase
+Definir parametros para la pila:
+    Tamaño
+    Punto/indice de inicio
+    pila vacia
+*/
+
+/*
+clase
+Funciones del PDA:
+    recorrido
+    desapilar
+    imprimir recorrido
+    transicion de estados
+    validacion
+*/
+
+// main que ejecute el programa
